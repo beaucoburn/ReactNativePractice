d937cb9101e63deaabc39d8fbe1b8ff8fdc2bf8d
beaucoburn <douglas.coburn@gmail.com>
2024-08-10 16:28:54 -0600

    modified:   rn-starter-boilerplate/rn-starter/App.js 	new file:   rn-starter-boilerplate/rn-starter/src/screens/ListScreen.js

---
+++ rn-starter-boilerplate/rn-starter/src/screens/ListScreen.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+
+const ListScreen = () => {
+  const friends = [
+    { name: "Friend #1" },
+    { name: "Friend #2" },
+    { name: "Friend #3" },
+    { name: "Friend #4" },
+    { name: "Friend #5" },
+    { name: "Friend #6" },
+    { name: "Friend #7" },
+    { name: "Friend #8" },
+    { name: "Friend #9" },
+  ];
+  return (
+    <View>
+      <Text>List Screen</Text>
+      <FlatList
+        data={friends}
+        renderItem={({ item }) => {
+          return <Text>{item.name}</Text>;
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+export default ListScreen;
