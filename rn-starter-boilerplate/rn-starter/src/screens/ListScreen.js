67e7fa65c7fa38655b3c3f4f8e4feb05d5a3266c
beaucoburn <douglas.coburn@gmail.com>
2024-08-10 18:14:47 -0600

    modified:   rn-starter-boilerplate/rn-starter/src/screens/ListScreen.js

--- rn-starter-boilerplate/rn-starter/src/screens/ListScreen.js
+++ rn-starter-boilerplate/rn-starter/src/screens/ListScreen.js
@@ -3,15 +3,15 @@ import { View, Text, StyleSheet, FlatList } from "react-native";
 
 const ListScreen = () => {
   const friends = [
-    { name: "Friend #1" },
-    { name: "Friend #2" },
-    { name: "Friend #3" },
-    { name: "Friend #4" },
-    { name: "Friend #5" },
-    { name: "Friend #6" },
-    { name: "Friend #7" },
-    { name: "Friend #8" },
-    { name: "Friend #9" },
+    { name: "Friend #1", key: "A" },
+    { name: "Friend #2", key: "B" },
+    { name: "Friend #3", key: "C" },
+    { name: "Friend #4", key: "D" },
+    { name: "Friend #5", key: "E" },
+    { name: "Friend #6", key: "F" },
+    { name: "Friend #7", key: "G" },
+    { name: "Friend #8", key: "H" },
+    { name: "Friend #9", key: "I" },
   ];
   return (
     <View>
