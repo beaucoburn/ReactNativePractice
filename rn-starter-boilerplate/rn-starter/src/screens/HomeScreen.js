151836798a9d4079d08f3f73be520741b49b9583
beaucoburn <douglas.coburn@gmail.com>
2024-08-13 18:26:29 -0600

    Add navigation to Button and TouchableOpacity

--- rn-starter-boilerplate/rn-starter/src/screens/HomeScreen.js
+++ rn-starter-boilerplate/rn-starter/src/screens/HomeScreen.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
+const HomeScreen = (props) => {
   const name = "Beau";
 
   return (
     <View>
       <Text style={styles.text}>Getting Started with React Native!</Text>
       <Text style={styles.subText}>My name is {name}</Text>
-      <Button title="Go to Component Screen" onPress={() => props.navigation.navigate("Component")} />
-      <TouchableOpacity>
+      <Button title="Go to Component Screen" onPress={() => props.navigation.navigate("Components")} />
+      <TouchableOpacity onPress={() => props.navigation.navigate("List") }>
         <Text style={styles.subText}>
           Go to the List Screen
         </Text>
