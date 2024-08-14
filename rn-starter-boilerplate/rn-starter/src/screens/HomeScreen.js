df3ded6b23581c1e3d4b26754d9e24ef5208f14d
beaucoburn <douglas.coburn@gmail.com>
2024-08-13 18:33:04 -0600

    Destructered the Props

--- rn-starter-boilerplate/rn-starter/src/screens/HomeScreen.js
+++ rn-starter-boilerplate/rn-starter/src/screens/HomeScreen.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = (props) => {
+const HomeScreen = ({ navigation }) => {
   const name = "Beau";
 
   return (
     <View>
       <Text style={styles.text}>Getting Started with React Native!</Text>
       <Text style={styles.subText}>My name is {name}</Text>
-      <Button title="Go to Component Screen" onPress={() => props.navigation.navigate("Components")} />
-      <TouchableOpacity onPress={() => props.navigation.navigate("List") }>
-        <Text style={styles.subText}>
-          Go to the List Screen
-        </Text>
-      </TouchableOpacity>
+      <Button
+        title="Go to Component Screen"
+        onPress={() => navigation.navigate("Components")}
+      />
+      <Button
+        title="Go to List Screen"
+        onPress={() => navigation.navigate("List")}
+      />
     </View>
   );
 };
