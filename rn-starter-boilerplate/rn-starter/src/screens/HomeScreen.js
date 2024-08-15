bf4c0918f95811dc9b2aa2bdf8435a2fc227151d
beaucoburn <douglas.coburn@gmail.com>
2024-08-15 17:16:58 -0600

    modified:   rn-starter-boilerplate/rn-starter/App.js 	modified:   rn-starter-boilerplate/rn-starter/src/screens/HomeScreen.js 	new file:   rn-starter-boilerplate/rn-starter/src/screens/ImageScreen.js

--- rn-starter-boilerplate/rn-starter/src/screens/HomeScreen.js
+++ rn-starter-boilerplate/rn-starter/src/screens/HomeScreen.js
@@ -16,6 +16,10 @@ const HomeScreen = ({ navigation }) => {
         title="Go to List Screen"
         onPress={() => navigation.navigate("List")}
       />
+      <Button
+        title="Go to Image Screen"
+        onPress={() => navigation.navigate("Image")}
+      />
     </View>
   );
 };
---
+++ rn-starter-boilerplate/rn-starter/src/screens/ImageScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ImageScreen = () => {
+  return (
+    <Text>Image Screen</Text>
+  )
+}
+
+export default ImageScreen;
+
+const styles = StyleSheet.create();
