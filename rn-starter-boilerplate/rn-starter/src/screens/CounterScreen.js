0620a26faa7a09409c8cd82a899bee023927c602
beaucoburn <douglas.coburn@gmail.com>
2024-08-16 17:32:41 -0600

    modified:   rn-starter-boilerplate/rn-starter/App.js 	new file:   rn-starter-boilerplate/rn-starter/src/screens/CounterScreen.js 	modified:   rn-starter-boilerplate/rn-starter/src/screens/HomeScreen.js

---
+++ rn-starter-boilerplate/rn-starter/src/screens/CounterScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+const CounterScreen = () => {
+  return (
+    <View>
+      <Text>Current count:</Text>
+    </View>
+  );
+};
+
+export default CounterScreen;
+
+const styles = StyleSheet.create({});
--- rn-starter-boilerplate/rn-starter/src/screens/HomeScreen.js
+++ rn-starter-boilerplate/rn-starter/src/screens/HomeScreen.js
@@ -20,6 +20,10 @@ const HomeScreen = ({ navigation }) => {
         title="Go to Image Screen"
         onPress={() => navigation.navigate("Image")}
       />
+      <Button
+        title="Go to Counter Screen"
+        onPress={() => navigation.navigate("Counter")}
+      />
     </View>
   );
 };
