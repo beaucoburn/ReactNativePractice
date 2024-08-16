79cc1edcb939dc1c299f095c36f69c980035bca7
beaucoburn <douglas.coburn@gmail.com>
2024-08-16 17:56:49 -0600

    modified:   rn-starter-boilerplate/rn-starter/src/screens/CounterScreen.js

--- rn-starter-boilerplate/rn-starter/src/screens/CounterScreen.js
+++ rn-starter-boilerplate/rn-starter/src/screens/CounterScreen.js
@@ -1,18 +1,23 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-
-
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 const CounterScreen = () => {
-  
   const [counter, setCounter] = useState(0);
- 
-  
 
   return (
     <View>
-      <Button title="Increase" onPress={() => {setCounter(counter + 1)}} />
-      <Button title="Decrease" onPress={() => {setCounter(counter - 1)}} />
+      <Button
+        title="Increase"
+        onPress={() => {
+          setCounter(counter + 1);
+        }}
+      />
+      <Button
+        title="Decrease"
+        onPress={() => {
+          setCounter(counter - 1);
+        }}
+      />
       <Text>Current Count: {counter}</Text>
     </View>
   );
