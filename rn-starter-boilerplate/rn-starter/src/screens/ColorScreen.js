d2ba46f41260184a0f54f8afa3e60aae12fd2699
beaucoburn <douglas.coburn@gmail.com>
2024-08-20 17:36:00 -0600

    modified:   rn-starter-boilerplate/rn-starter/src/screens/ColorScreen.js 	modified:   rn-starter-boilerplate/rn-starter/src/screens/HomeScreen.js

--- rn-starter-boilerplate/rn-starter/src/screens/ColorScreen.js
+++ rn-starter-boilerplate/rn-starter/src/screens/ColorScreen.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Button, StyleSheet, Text } from "react-native";
 
 const ColorScreen = () => {
+  const [colors, setColors] = useState([]);
+
   return (
     <View>
       <Text>This is a color screen</Text>
-      <Button title="Add a Color" />
+      <Button title="Add a Color" onPress={() => {
+        setColors([...colors, randomRgb()]);
+      }} />
+      <View
+        style={{ height: 100, width: 100, backgroundColor: randomRgb() }}
+      />
     </View>
   );
 };
+
+const randomRgb = () => {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  return `rgb(${red}, ${green}, ${blue})`;
+}
 
 export default ColorScreen;
 
--- rn-starter-boilerplate/rn-starter/src/screens/HomeScreen.js
+++ rn-starter-boilerplate/rn-starter/src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
 
 const HomeScreen = ({ navigation }) => {
   const name = "Beau";
