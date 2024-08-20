0da76ee96bbc336873112cfa7ab49a30eb957e18
beaucoburn <douglas.coburn@gmail.com>
2024-08-20 17:24:48 -0600

    modified:   rn-starter-boilerplate/rn-starter/App.js 	modified:   rn-starter-boilerplate/rn-starter/src/screens/ColorScreen.js 	modified:   rn-starter-boilerplate/rn-starter/src/screens/HomeScreen.js

---
+++ rn-starter-boilerplate/rn-starter/src/screens/ColorScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Button, StyleSheet, Text } from "react-native";
+
+const ColorScreen = () => {
+  return (
+    <View>
+      <Text>This is a color screen</Text>
+      <Button title="Add a Color" />
+    </View>
+  );
+};
+
+export default ColorScreen;
+
+const styles = StyleSheet.create({});
--- rn-starter-boilerplate/rn-starter/src/screens/HomeScreen.js
+++ rn-starter-boilerplate/rn-starter/src/screens/HomeScreen.js
@@ -24,6 +24,10 @@ const HomeScreen = ({ navigation }) => {
         title="Go to Counter Screen"
         onPress={() => navigation.navigate("Counter")}
       />
+      <Button
+        title="Go to Color Screen"
+        onPress={() => navigation.navigate("Color")}
+      />
     </View>
   );
 };
