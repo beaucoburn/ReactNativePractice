724a58a99c7d8f18ee83c7268ed8edc533ec2cc6
beaucoburn <douglas.coburn@gmail.com>
2024-08-24 18:37:19 -0600

    modified:   rn-starter-boilerplate/rn-starter/src/components/ColorCounter.js 	modified:   rn-starter-boilerplate/rn-starter/src/screens/SquareScreen.js

--- rn-starter-boilerplate/rn-starter/src/components/ColorCounter.js
+++ rn-starter-boilerplate/rn-starter/src/components/ColorCounter.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
-const ColorCounter = ({ color }) => {
+const ColorCounter = ({ color, onIncrease, onDecrease }) => {
   return (
     <View>
       <Text>{color}</Text>
-      <Button title={`Increase ${color}`} onChange={() => {}} />
-      <Button title={`Decrease ${color}`} /> 
+      <Button title={`Increase ${color}`} onPress={() => onIncrease()} />
+      <Button title={`Decrease ${color}`} onPress={() => onDecrease()} /> 
     </View>
   );
 };
--- rn-starter-boilerplate/rn-starter/src/screens/SquareScreen.js
+++ rn-starter-boilerplate/rn-starter/src/screens/SquareScreen.js
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import ColorCounter from "../components/ColorCounter";
 
-
-
 const SquareScreen = () => {
   const [red, setRed] = useState(0);
   const [green, setGreen] = useState(0);
@@ -11,9 +9,21 @@ const SquareScreen = () => {
 
   return (
     <View>
-      <ColorCounter color="Red" />
-      <ColorCounter color="Green" />
-      <ColorCounter color="Blue" />
+      <ColorCounter
+        onIncrease={() => setRed(red + 1)}
+        onDecrease={() => setRed(red - 1)}
+        color="Red"
+      />
+      <ColorCounter
+        onIncrease={() => setGreen(green + 1)}
+        onDecrease={() => setGreen(green - 1)}
+        color="Green"
+      />
+      <ColorCounter
+        onIncrease={() => setBlue(blue + 1)}
+        onDecrease={() => setBlue(blue - 1)}
+        color="Blue"
+      />
     </View>
   );
 };
