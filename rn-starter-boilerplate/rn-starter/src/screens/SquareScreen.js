9d338f842656366635cabb72569a8c12a571d013
beaucoburn <douglas.coburn@gmail.com>
2024-08-24 18:45:11 -0600

    modified:   rn-starter-boilerplate/rn-starter/src/screens/SquareScreen.js

--- rn-starter-boilerplate/rn-starter/src/screens/SquareScreen.js
+++ rn-starter-boilerplate/rn-starter/src/screens/SquareScreen.js
@@ -24,6 +24,13 @@ const SquareScreen = () => {
         onDecrease={() => setBlue(blue - 1)}
         color="Blue"
       />
+      <View
+        style={{
+          height: 150,
+          width: 150,
+          backgroundColor: `rgb(${red}, ${green}, ${blue})`,
+        }}
+      />
     </View>
   );
 };
