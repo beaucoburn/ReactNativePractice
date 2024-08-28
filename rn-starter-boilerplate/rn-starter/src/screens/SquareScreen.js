9b90d647c88461348831d5da01aa586a44504492
beaucoburn <douglas.coburn@gmail.com>
2024-08-27 18:26:45 -0600

    modified:   rn-starter-boilerplate/rn-starter/src/screens/SquareScreen.js

--- rn-starter-boilerplate/rn-starter/src/screens/SquareScreen.js
+++ rn-starter-boilerplate/rn-starter/src/screens/SquareScreen.js
@@ -22,25 +22,25 @@ const SquareScreen = () => {
   return (
     <View>
       <ColorCounter
-        onIncrease={() => }
-        onDecrease={() => }
+        onIncrease={() => dispatch({ colorToChange: 'red', amount: COLOR_INCREMENT }) }
+        onDecrease={() => dispatch({ colorToChange: 'red', amount: -1 * COLOR_INCREMENT }) }
         color="Red"
       />
       <ColorCounter
-        onIncrease={() => }
-        onDecrease={() => }
+        onIncrease={() => dispatch({ colorToChange: 'green', amount: COLOR_INCREMENT }) }
+        onDecrease={() => dispatch({ colorToChange: 'green', amount: -1 * COLOR_INCREMENT }) }
         color="Green"
       />
       <ColorCounter
-        onIncrease={() => }
-        onDecrease={() => }
+        onIncrease={() => dispatch({ colorToChange: 'blue', amount: COLOR_INCREMENT }) }
+        onDecrease={() => dispatch({ colorToChange: 'blue', amount: -1 * COLOR_INCREMENT }) }
         color="Blue"
       />
       <View
         style={{
           height: 150,
           width: 150,
-          backgroundColor: `rgb(${red}, ${green}, ${blue})`,
+          backgroundColor: `rgb(${state.red}, ${state.green}, ${state.blue})`,
         }}
       />
     </View>
