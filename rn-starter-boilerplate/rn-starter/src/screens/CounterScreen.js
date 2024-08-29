7eb9008c5488ff602e904602c9a12886b5b05d1c
beaucoburn <douglas.coburn@gmail.com>
2024-08-28 18:07:44 -0600

    modified:   rn-starter-boilerplate/rn-starter/src/screens/CounterScreen.js 	modified:   rn-starter-boilerplate/rn-starter/src/screens/SquareScreen.js

--- rn-starter-boilerplate/rn-starter/src/screens/CounterScreen.js
+++ rn-starter-boilerplate/rn-starter/src/screens/CounterScreen.js
@@ -5,30 +5,33 @@ const COUNTER_INCREMENT = 1;
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'change_increase':
+    case "change_increase":
       return { ...state, counter: state.counter + action.payload };
-    case 'change_decrease':
+    case "change_decrease":
       return { ...state, counter: state.counter + action.payload };
     default:
       return state;
   }
-}
+};
 
 const CounterScreen = () => {
   const [state, dispatch] = useReducer(reducer, { counter: 0 });
-  
+
   return (
     <View>
       <Button
         title="Increase"
         onPress={() => {
-          dispatch({ type: 'change_increase', payload: COUNTER_INCREMENT });
+          dispatch({ type: "change_increase", payload: COUNTER_INCREMENT });
         }}
       />
       <Button
         title="Decrease"
         onPress={() => {
-          dispatch({ type: 'change_decrease', payload: -1 * COUNTER_INCREMENT });
+          dispatch({
+            type: "change_decrease",
+            payload: -1 * COUNTER_INCREMENT,
+          });
         }}
       />
       <Text>Current Count: {state.counter}</Text>
