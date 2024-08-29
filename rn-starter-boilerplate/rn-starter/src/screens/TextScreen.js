79fd469c728adaf8778245ac02218d9a7ab7276c
beaucoburn <douglas.coburn@gmail.com>
2024-08-29 17:27:26 -0600

    modified:   rn-starter-boilerplate/rn-starter/src/screens/TextScreen.js

--- rn-starter-boilerplate/rn-starter/src/screens/TextScreen.js
+++ rn-starter-boilerplate/rn-starter/src/screens/TextScreen.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 
 const TextScreen = () => {
   return (
     <View>
-      <Text>
-        Text Screen
-      </Text>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
     </View>
   );
 };
 
-//const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    margin: 15,
+    borderColor: "black",
+    borderWidth: 1,
+  },
+});
 
 export default TextScreen;
