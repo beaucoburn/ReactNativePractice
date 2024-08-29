5ebb379fdd6954b0b9192bbaf54d0e98cfd95cf4
beaucoburn <douglas.coburn@gmail.com>
2024-08-29 17:51:08 -0600

    modified:   rn-starter-boilerplate/rn-starter/src/screens/TextScreen.js

--- rn-starter-boilerplate/rn-starter/src/screens/TextScreen.js
+++ rn-starter-boilerplate/rn-starter/src/screens/TextScreen.js
@@ -2,19 +2,19 @@ import React, { useState } from "react";
 import { View, Text, StyleSheet, TextInput } from "react-native";
 
 const TextScreen = () => {
-  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <View>
-      <Text>Type your name:</Text>
+      <Text>Enter Password:</Text>
       <TextInput
         style={styles.input}
         autoCapitalize="none"
         autoCorrect={false}
-        value={name}
-        onChangeText={(newValue) => setName(newValue)}
+        value={password}
+        onChangeText={(newValue) => setPassword(newValue)}
       />
-    <Text>My name is {name}</Text>
+    {password.length < 5 ? <Text>Password needs to be at least 5 characters</Text> : ""}
     </View>
   );
 };
