c033d608c43c1ee80965fa69902abc794510f5ca
beaucoburn <douglas.coburn@gmail.com>
2024-08-30 17:32:50 -0600

    Update HomeScreen

--- rn-starter-boilerplate/rn-starter/src/screens/HomeScreen.js
+++ rn-starter-boilerplate/rn-starter/src/screens/HomeScreen.js
@@ -36,6 +36,10 @@ const HomeScreen = ({ navigation }) => {
         title="Go to Text Screen"
         onPress={() => navigation.navigate("Text")}
       />
+      <Button
+        title="Go to Box Screen"
+        onPress={() => navigation.navigate("Box")}
+      />
     </View>
   );
 };
