2ea203f274d7bc460c20ad02c205b558128f4e9f
beaucoburn <douglas.coburn@gmail.com>
2024-08-30 17:43:28 -0600

    Added Borders

--- rn-starter-boilerplate/rn-starter/src/screens/BoxScreen.js
+++ rn-starter-boilerplate/rn-starter/src/screens/BoxScreen.js
@@ -3,12 +3,21 @@ import { View, Text, StyleSheet } from 'react-native';
 
 const BoxScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text>Box Screen</Text>
+    <View style={styles.viewStyle}>
+      <Text style={styles.textStyle}>Box Screen</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  viewStyle: {
+    borderWidth: 1,
+    borderColor: 'black',
+  },
+  textStyle: {
+    borderWidth: 1,
+    borderColor: 'red',
+  },
+});
 
 export default BoxScreen;
