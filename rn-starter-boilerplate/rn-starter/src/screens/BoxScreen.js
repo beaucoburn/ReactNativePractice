f9afdf2bb87db7a4b897d616ba3a860eef57b55d
beaucoburn <douglas.coburn@gmail.com>
2024-08-30 17:19:51 -0600

    add BoxScreen

---
+++ rn-starter-boilerplate/rn-starter/src/screens/BoxScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const BoxScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Box Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({}):
+
+export default BoxScreen;
