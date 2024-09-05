100db1288c277de1698cf9d4e220c55129fbc600
beaucoburn <douglas.coburn@gmail.com>
2024-09-04 18:07:01 -0600

    new file:   food/src/components/RestaurantList.js 	new file:   food/src/components/SearchBar.js

---
+++ food/src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
+
+const SearchBar = () => {
+  return (
+    <View style={styles.searchBar}>
+      <Text style={styles.searchBarText}>Search</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({});
+
+export default SearchBar;
