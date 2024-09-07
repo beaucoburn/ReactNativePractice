46b74af4687f652cb905f54067fc0633cbe65aa5
beaucoburn <douglas.coburn@gmail.com>
2024-09-06 18:41:07 -0600

    modified:   food/src/components/SearchBar.js

--- food/src/components/SearchBar.js
+++ food/src/components/SearchBar.js
@@ -12,8 +12,8 @@ const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
         style={styles.inputStyle} 
         placeholder="Search" 
         value={term} 
-        onChangeText={newTerm => onTermChange(newTerm)} 
-        onEndEditing={() => onTermSubmit() }
+        onChangeText={onTermChange}
+        onEndEditing={onTermSubmit}
       />
     </View>
   )
