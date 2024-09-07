bc1dd43f104010a6885838293763945b55e36605
beaucoburn <douglas.coburn@gmail.com>
2024-09-06 18:29:36 -0600

    modified:   food/src/components/SearchBar.js 	modified:   food/src/screens/SearchScreen.js

--- food/src/components/SearchBar.js
+++ food/src/components/SearchBar.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { View, TextInput, StyleSheet } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange }) => {
   return (
     <View style={styles.backgroundStyle}>
       <Feather name="search" style={styles.iconStyle} />
-      <TextInput style={styles.inputStyle} placeholder="Search" />
+      <TextInput 
+        style={styles.inputStyle} 
+        placeholder="Search" 
+        value={term} 
+        onTermChangeText={newTerm => onTermChange(newTerm)} 
+      />
     </View>
   )
 }
--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
+  const [term, setTerm] = useState('');
+
   return (
     <View>
-      <SearchBar />
+      <SearchBar term={term} onTermChange={(newTerm) => setTerm(newTerm)} />
       <Text>Search Screen</Text>
     </View>
   );
