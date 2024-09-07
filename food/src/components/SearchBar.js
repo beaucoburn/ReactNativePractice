095929c0bd63852ef19cf560e5a2e8d14c7e887d
beaucoburn <douglas.coburn@gmail.com>
2024-09-06 18:14:47 -0600

    modified:   food/src/components/SearchBar.js

--- food/src/components/SearchBar.js
+++ food/src/components/SearchBar.js
@@ -13,6 +13,7 @@ const SearchBar = () => {
 
 const styles = StyleSheet.create({
   backgroundStyle: {
+    marginTop: 10,
     backgroundColor: '#F0EEEE',
     height: 50,
     borderRadius: 5,
@@ -20,13 +21,13 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
   },
   inputStyle: {
-    borderColor: 'black',
-    borderWidth: 1,
     flex: 1,
+    fontSize: 18,
   },
   iconStyle: {
     fontSize: 35,
     alignSelf: 'center',
+    marginHorizontal: 15,
   }
 });
 
