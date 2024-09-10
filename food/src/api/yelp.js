b4a58ffe9a357aaf030c304d3542a73fcee99a06
beaucoburn <douglas.coburn@gmail.com>
2024-09-10 17:37:41 -0600

    new file:   food/.env 	modified:   food/package-lock.json 	modified:   food/package.json 	new file:   food/src/api/yelp.js 	modified:   food/src/screens/SearchScreen.js

---
+++ food/src/api/yelp.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'https://api.yelp.com/v3/businesses',
+  headers: {
+    Authorization: 'Bearer GyDhAeLE5wC7Pppmq_2gemKJiqtnN4Wwj3vJKk74skzUsiZh19GProVSJ6QWX9I3S6p5E-UtTEyW-3dk8UOAVdITb-vjgmIKG40rFEhdoqVnB3Pn_zJt0Wb0iEWQZnYx'
+  }
+});
+
+
--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,14 +1,28 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
+
+  const searchApi = async () => {
+    const response = await yelp.get('/search', {
+      params: {
+        term,
+        location: 'San Francisco, CA',
+        limit: 50,
+      }
+    });
+    setResults(response.data.businesses);
+  }
 
   return (
     <View>
-      <SearchBar term={term} onTermChange={(newTerm) => setTerm(newTerm)} onTermSubmit={() => } />
+      <SearchBar term={term} onTermChange={(newTerm) => setTerm(newTerm)} onTermSubmit={() =>  console.log('submitted')} />
       <Text>Search Screen</Text>
+      <Text> We have found {results.length} results</Text>
     </View>
   );
 };
