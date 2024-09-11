c2c89673b5c72f56cb84737a3d7599cd5d536446
beaucoburn <douglas.coburn@gmail.com>
2024-09-11 17:42:38 -0600

    modified:   food/src/screens/SearchScreen.js

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
