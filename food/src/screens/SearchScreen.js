04c7d4fc88618b217e4e48e43e0bf2948c462482
beaucoburn <douglas.coburn@gmail.com>
2024-09-10 18:23:37 -0600

    modified:   food/src/screens/SearchScreen.js

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -8,11 +8,11 @@ const SearchScreen = () => {
   const [results, setResults] = useState([]);
   const [errorMessage, setErrorMessage] = useState('');
 
-  const searchApi = async () => {
+  const searchApi = async (searchTerm) => {
     try {
     const response = await yelp.get('/search', {
       params: {
-        term,
+        term: searchTerm,
         location: 'San Francisco, CA',
         limit: 50,
       }
@@ -25,7 +25,7 @@ const SearchScreen = () => {
 
   return (
     <View>
-      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={() => searchApi(term)} />
         {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text> We have found {results.length} results</Text>
     </View>
