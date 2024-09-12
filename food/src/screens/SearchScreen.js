e238e22a3a1d86cfba7950f1fb563bbded75d770
beaucoburn <douglas.coburn@gmail.com>
2024-09-11 18:12:13 -0600

    modified:   food/src/screens/SearchScreen.js

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,15 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
-  const [term, setTerm] = useState('');
- 
+  const [term, setTerm] = useState("");
+
   return (
     <View>
-      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={() => searchApi(term)} />
-        {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
+      />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text> We have found {results.length} results</Text>
     </View>
   );
