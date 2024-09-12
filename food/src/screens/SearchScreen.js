b865a85f36ecb2607cf1579f184b8b6fd34e2800
beaucoburn <douglas.coburn@gmail.com>
2024-09-12 17:49:49 -0600

    Modified SearchScreen.js

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
 import useResults from "../hooks/useResults";
+import ResultsList from "../components/ResultsList";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
@@ -16,6 +17,8 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text> We have found {results.length} results</Text>
+      <ResultsList />
+      <ResultsList />
     </View>
   );
 };
