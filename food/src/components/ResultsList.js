c854fe4d0636fb122cc8aad1942627e1f4284553
beaucoburn <douglas.coburn@gmail.com>
2024-09-12 17:49:21 -0600

    Add ResultsList.js and Modified SearchScreen.js

---
+++ food/src/components/ResultsList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ResultsList = () => {
+  return (
+    <View style={styles.searchBar}>
+      <Text style={styles.searchBarText}>Results List</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({});
+
+export default ResultsList;
