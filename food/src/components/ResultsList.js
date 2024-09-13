c06f0f6b985fc39cb4d959bcf0cd654475ef9318
beaucoburn <douglas.coburn@gmail.com>
2024-09-12 18:21:48 -0600

    modified:   food/src/components/ResultsList.js

--- food/src/components/ResultsList.js
+++ food/src/components/ResultsList.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
-const ResultsList = ({ title}) => {
+const ResultsList = ({ title }) => {
   return (
     <View style={styles.searchBar}>
       <Text style={styles.titleStyle}>{title}</Text>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   titleStyle: {
     fontSize: 18,
-    fontWeight: 'bold',
-  }
+    fontWeight: "bold",
+  },
 });
 
 export default ResultsList;
