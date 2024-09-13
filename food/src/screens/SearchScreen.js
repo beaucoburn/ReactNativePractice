80fe291214a66e014bd30a75ea5c51f8ed3ec70a
beaucoburn <douglas.coburn@gmail.com>
2024-09-12 18:26:49 -0600

    modified:   food/src/screens/SearchScreen.js

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -8,6 +8,12 @@ const SearchScreen = () => {
   const [term, setTerm] = useState("");
   const [searchApi, results, errorMessage] = useResults();
 
+  const filterResultsByPrice = (price) => {
+    return results.filter(result => {
+      return result.price === price
+    })
+  }
+
   return (
     <View>
       <SearchBar
@@ -17,9 +23,9 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text> We have found {results.length} results</Text>
-      <ResultsList title="Cost Effective" />
-      <ResultsList title="Bit Pricier" />
-      <ResultsList title="Big Spender" />
+      <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
+      <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
+      <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
     </View>
   );
 };
