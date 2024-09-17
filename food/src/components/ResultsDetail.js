1f7701addefc0be2999d0469d801e884683591c1
beaucoburn <douglas.coburn@gmail.com>
2024-09-17 17:47:27 -0600

    modified:   food/src/components/ResultsDetail.js

--- food/src/components/ResultsDetail.js
+++ food/src/components/ResultsDetail.js
@@ -4,10 +4,25 @@ import { View, Text, StyleSheet, Image } from "react-native";
 const ResultsDetail = ({ result }) => {
   return (
     <View>
-      <Text>{result.name}</Text>
+      <Image
+        source={{ uri: result.image_url }} 
+        style={styles.image}
+      />
+      <Text style={styles.name}>{result.name}</Text>
+      <Text>{result.rating} Stars, {result.review_count} Reviews</Text>
     </View>
   )
 }
 
 export default ResultsDetail;
 
+const styles = StyleSheet.create({
+  image: {
+    width: 250,
+    height: 120,
+    borderRadius: 4,
+  },
+  name: {
+    fontWeight: "bold",
+  }
+});
