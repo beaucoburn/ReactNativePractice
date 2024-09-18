4b2b30f6dcce24b1c8f3e5a2d18d635e2eb9d325
beaucoburn <douglas.coburn@gmail.com>
2024-09-17 18:13:18 -0600

    modified:   food/src/screens/SearchScreen.js

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -15,7 +15,7 @@ const SearchScreen = () => {
   };
 
   return (
-    <View style={{ flex: 1 }}>
+    <>
       <SearchBar
         term={term}
         onTermChange={setTerm}
@@ -34,7 +34,7 @@ const SearchScreen = () => {
           title="Big Spender"
         />
       </ScrollView>
-    </View>
+    </>
   );
 };
 
