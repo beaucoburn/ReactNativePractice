82aa989f3a04042b334b6031f2a09b0cd36f5e2b
beaucoburn <douglas.coburn@gmail.com>
2024-09-17 18:16:20 -0600

    modified:   food/src/components/SearchBar.js 	modified:   food/src/screens/SearchScreen.js

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -22,7 +22,6 @@ const SearchScreen = () => {
         onTermSubmit={() => searchApi(term)}
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
-      <Text> We have found {results.length} results</Text>
       <ScrollView>
         <ResultsList
           results={filterResultsByPrice("$")}
