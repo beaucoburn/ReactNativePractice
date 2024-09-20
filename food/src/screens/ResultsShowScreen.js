718d3abf39a0feb64dfd16b225d41478fd5e06c8
beaucoburn <douglas.coburn@gmail.com>
2024-09-20 17:47:41 -0600

    modified:   food/src/screens/ResultsShowScreen.js

--- food/src/screens/ResultsShowScreen.js
+++ food/src/screens/ResultsShowScreen.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList, Image } from "react-native";
 import yelp from "../api/yelp";
 
 const ResultsShowScreen = ({ navigation }) => {
   const [result, setResult] = useState(null);
-  const id = navigation.getParam('id');
-  
+  const id = navigation.getParam("id");
+
   const getResult = async (id) => {
-   const response = await yelp.get(`/${id}`); 
+    const response = await yelp.get(`/${id}`);
     setResult(response.data);
   };
 
@@ -22,10 +22,22 @@ const ResultsShowScreen = ({ navigation }) => {
   return (
     <View>
       <Text>{result.name}</Text>
+      <FlatList
+        data={result.photos}
+        keyExtractor={(photo) => photo}
+        renderItem={({ item }) => {
+          return <Image style={styles.image} source={{ uri: item }} />;
+        }}
+      />
     </View>
   );
 };
 
 export default ResultsShowScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    height: 200,
+    width: 300,
+  }
+});
