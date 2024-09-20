a650763a458e9df0fe12e94d44d10323b678b89e
beaucoburn <douglas.coburn@gmail.com>
2024-09-19 18:34:42 -0600

    modified:   food/src/api/yelp.js

--- food/src/api/yelp.js
+++ food/src/api/yelp.js
@@ -4,6 +4,5 @@ export default axios.create({
   baseURL: "https://api.yelp.com/v3/businesses",
   headers: {
     Authorization:
-      "Bearer MnDKlw8cBLrcKUrTgdSds7nnG3669ASDwl_9_lQ4bLPAPJGZGx6VCCsy5twMGSHvL5MpM6ZvzZowVRLzj43-1A4fJcKulFfltTmqBCCPoBHvMx4DuKvA9CwGoMDsZnYx",
-  },
+      "Bearer a3h4JLwYlgt9vcKpeAeR-UrYkAmMZyYSpyHan-xy8UMocvzRAQGu_LeevTREL11Vg8Znup2gmrrtb3YE0MKnRB5lHxRuXqkJMM58KMsTEcEb4CQeX91zPtJQ40zqZnYx"  },
 });
