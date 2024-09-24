170c1e5e3036536f1bcc8e0b515a636eefc1d1c8
beaucoburn <douglas.coburn@gmail.com>
2024-09-24 17:59:51 -0600

    modified:   blog/App.js 	renamed:    blog/src/IndexScreen.js -> blog/src/screens/IndexScreen.js

--- blog/App.js
+++ blog/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
-import IndexScreen from './src/IndexScreen';
+import IndexScreen from './src/screens/IndexScreen';
 
 const navigator = createStackNavigator({
   Index: IndexScreen,
