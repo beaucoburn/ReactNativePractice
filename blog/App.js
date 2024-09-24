1eb9d0ed0179606182e12540c34353299a1973d0
beaucoburn <douglas.coburn@gmail.com>
2024-09-24 17:52:52 -0600

    modified:   blog/App.js

--- blog/App.js
+++ blog/App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import IndexScreen from './src/IndexScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const navigator = createStackNavigator({
+  Index: IndexScreen,
+}, {
+  initialRouteName: 'Index',
+  defaultNavigationOptions: {
+    title: 'Blogs',
   },
 });
+
+const App = createAppContainer(navigator);
+
+export default () => {
+  return <App />;
+};
