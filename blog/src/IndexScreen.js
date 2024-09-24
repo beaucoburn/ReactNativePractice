e8e70076ba61f6ca7152a54705cd355f18cf1b1f
beaucoburn <douglas.coburn@gmail.com>
2024-09-24 17:48:41 -0600

    Modified: App.js

---
+++ blog/src/IndexScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const IndexScreen = () => {
+  return (
+    <View>
+      <Text>Hello, world!</Text>
+    </View>
+  );
+};
+
+export default IndexScreen;
+
+const styles = StyleSheet.create({});
