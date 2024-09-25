b369a0745a25ef3d17d361da31dc4f5b8d94f266
beaucoburn <douglas.coburn@gmail.com>
2024-09-24 18:09:56 -0600

    modified:   blog/App.js 	new file:   blog/src/context/BlogContext.js

---
+++ blog/src/context/BlogContext.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BlogContext = React.createContext();
+
+export const BlogProvider = ({ children }) => {
+  return (
+    <BlogContext.Provider>
+      {children}
+    </BlogContext.Provider>
+  );
+}
