be1ba7d561253178cb8842670d288019866bb193
beaucoburn <douglas.coburn@gmail.com>
2024-09-25 17:44:28 -0600

    modified:   blog/src/context/BlogContext.js 	modified:   blog/src/screens/IndexScreen.js

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -3,8 +3,16 @@ import React from 'react';
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
+  const blogPosts = [
+    {title: 'Blog Post #1'},
+    {title: 'Blog Post #2'},
+    {title: 'Blog Post #3'},
+    {title: 'Blog Post #4'},
+    {title: 'Blog Post #5'},
+  ];
+
   return (
-    <BlogContext.Provider value={5}>
+    <BlogContext.Provider value={blogPosts}>
       {children}
     </BlogContext.Provider>
   );
