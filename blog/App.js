44235d3d4e9435798d3d8b8f4ac8ba5d88ac8828
beaucoburn <douglas.coburn@gmail.com>
2024-09-25 17:55:48 -0600

    modified:   blog/App.js 	modified:   blog/src/screens/IndexScreen.js

--- blog/App.js
+++ blog/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import { createStackNavigator } from 'react-navigation-stack';
-import { createAppContainer } from 'react-navigation';
-import IndexScreen from './src/screens/IndexScreen';
-import { BlogProvider } from './src/context/BlogContext';
+import React from "react";
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+import IndexScreen from "./src/screens/IndexScreen";
+import { BlogProvider } from "./src/context/BlogContext";
 
-const navigator = createStackNavigator({
-  Index: IndexScreen,
-}, {
-  initialRouteName: 'Index',
-  defaultNavigationOptions: {
-    title: 'Blogs',
+const navigator = createStackNavigator(
+  {
+    Index: IndexScreen,
   },
-});
+  {
+    initialRouteName: "Index",
+    defaultNavigationOptions: {
+      title: "Blogs",
+    },
+  },
+);
 
 const App = createAppContainer(navigator);
 
--- blog/src/screens/IndexScreen.js
+++ blog/src/screens/IndexScreen.js
@@ -1,6 +1,6 @@
-import React, { useContext } from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
-import BlogContext from '../context/BlogContext';
+import React, { useContext } from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import BlogContext from "../context/BlogContext";
 
 const IndexScreen = () => {
   const blogPosts = useContext(BlogContext);
