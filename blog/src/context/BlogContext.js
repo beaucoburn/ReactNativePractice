8eca0225c1df1e7dc9af890dfc3bb0e71de649c8
beaucoburn <douglas.coburn@gmail.com>
2024-09-25 17:53:04 -0600

    modified:   blog/src/context/BlogContext.js

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -1,22 +1,20 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-  const blogPosts = [
-    {title: 'Blog Post #1'},
-    {title: 'Blog Post #2'},
-    {title: 'Blog Post #3'},
-    {title: 'Blog Post #4'},
-    {title: 'Blog Post #5'},
-  ];
+  const [blogPosts, setBlogPosts] = useState([]);
+
+  const addBlogPost = () => {
+    setBlogPosts([
+      ...blogPosts,
+      { title: `Blog Post #{blogPosts.length + 1}` },
+    ]);
+  };
 
   return (
-    <BlogContext.Provider value={blogPosts}>
-      {children}
-    </BlogContext.Provider>
+    <BlogContext.Provider value={blogPosts}>{children}</BlogContext.Provider>
   );
-}
+};
 
 export default BlogContext;
-
