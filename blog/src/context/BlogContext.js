e7a77865a1fd8fb8c404340d1a0b54a4894aacf0
beaucoburn <douglas.coburn@gmail.com>
2024-09-26 17:46:42 -0600

    modified:   blog/src/context/BlogContext.js 	new file:   blog/src/context/createDataContext.js

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -1,28 +1,20 @@
-import React, { useReducer } from "react";
-
-const BlogContext = React.createContext();
+import createDataContext from "./createDataContext";
 
 const blogReducer = (state, action) => {
   switch (action.type) {
     case "add_blogpost":
-      return [...state, { title: `Blog Post #${state.length + 1}`}];
+      return [...state, { title: `Blog Post #${state.length + 1}` }];
     default:
       return state;
   }
 };
 
-export const BlogProvider = ({ children }) => {
-  const [blogPosts, dispatch] = useReducer(blogReducer, []);
-
-  const addBlogPost = () => {
-    dispatch({ type: "add_blogpost" });
-  };
-
-  return (
-    <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
-      {children}
-    </BlogContext.Provider>
-  );
+const addBlogPost = () => {
+  dispatch({ type: "add_blogpost" });
 };
 
-export default BlogContext;
+export const { Context, Provider } = createDataContext(
+  blogReducer,
+  { addBlogPost },
+  [],
+);
