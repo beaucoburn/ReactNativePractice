c8f02f7de4d455bbc0271c5ae98cc4bd26efe15b
beaucoburn <douglas.coburn@gmail.com>
2024-09-26 18:04:14 -0600

    modified:   blog/App.js 	modified:   blog/src/screens/IndexScreen.js

--- blog/App.js
+++ blog/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
 import IndexScreen from "./src/screens/IndexScreen";
-import { BlogProvider } from "./src/context/BlogContext";
+import { Provider } from "./src/context/BlogContext";
 
 const navigator = createStackNavigator(
   {
@@ -20,8 +20,8 @@ const App = createAppContainer(navigator);
 
 export default () => {
   return (
-    <BlogProvider>
+    <Provider>
       <App />
-    </BlogProvider>
+    </Provider>
   );
 };
