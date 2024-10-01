d3efc02d37b82a99b11f80f9c3e3ef315cac7b6b
beaucoburn <douglas.coburn@gmail.com>
2024-10-01 17:11:02 -0600

    modified:   blog/src/context/BlogContext.js 	modified:   blog/src/screens/IndexScreen.js

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -2,6 +2,8 @@ import createDataContext from "./createDataContext";
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case "delete_blogpost":
+      return state.filter((blogPost) => blogPost.id !== action.payload);
     case "add_blogpost":
       return [
         ...state,
@@ -21,8 +23,14 @@ const addBlogPost = (dispatch) => {
   };
 };
 
+const deleteBlogPost = (dispatch) => {
+  return (id) => {
+    dispatch({ type: "delete_blogpost", payload: id })
+  };
+}
+
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost },
+  { addBlogPost, deleteBlogPost },
   [],
 );
