7743c17b42153fcfe770c3843dac459b47e3377d
beaucoburn <douglas.coburn@gmail.com>
2024-10-01 17:01:05 -0600

    modified:   blog/src/context/BlogContext.js 	modified:   blog/src/screens/IndexScreen.js

--- blog/src/screens/IndexScreen.js
+++ blog/src/screens/IndexScreen.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { View, Text, StyleSheet, FlatList, Button } from "react-native";
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from "react-native";
 import { Context } from "../context/BlogContext";
 import { Feather } from "@expo/vector-icons";
 
@@ -15,8 +15,10 @@ const IndexScreen = () => {
         renderItem={({ item }) => {
           return (
             <View style={styles.row}>
-              <Text style={styles.title}>{item.title}</Text>
+              <Text style={styles.title}>{item.title} - {item.id}</Text>
+              <TouchableOpacity onPress={() => console.log(item.id)} >
               <Feather style={styles.icon} name="trash" />
+              </TouchableOpacity>
             </View>
           );
         }}
