3e930ad75bffca2e6ac59b96c15d19dfac2b1018
beaucoburn <douglas.coburn@gmail.com>
2024-10-02 17:26:47 -0600

    modified:   blog/src/screens/ShowScreen.js

--- blog/src/screens/ShowScreen.js
+++ blog/src/screens/ShowScreen.js
@@ -1,17 +1,19 @@
-import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Context } from '../context/BlogContext';
+import React, { useContext } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Context } from "../context/BlogContext";
 
 const ShowScreen = ({ navigation }) => {
   const { state } = useContext(Context);
 
-  const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam('id'));
+  const blogPost = state.find(
+    (blogPost) => blogPost.id === navigation.getParam("id"),
+  );
 
   return (
     <View>
       <Text>{blogPost.title}</Text>
     </View>
-  )
+  );
 };
 
 const styles = StyleSheet.create({});
