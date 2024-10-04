96e24b087177d4e92ea209ac0c5dab34160c9daf
beaucoburn <douglas.coburn@gmail.com>
2024-10-04 17:55:22 -0600

    modified:   blog/src/screens/IndexScreen.js

--- blog/src/screens/IndexScreen.js
+++ blog/src/screens/IndexScreen.js
@@ -15,7 +15,6 @@ const IndexScreen = ({ navigation }) => {
 
   return (
     <View>
-      <Button title="Add Post" onPress={addBlogPost} />
       <FlatList
         data={state}
         keyExtractor={(blogPost) => blogPost.title}
