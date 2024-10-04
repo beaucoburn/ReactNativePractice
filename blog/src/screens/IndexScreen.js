34f09176c5e8d18d87c09b555d6e61e46efbe6eb
beaucoburn <douglas.coburn@gmail.com>
2024-10-03 18:19:54 -0600

    modified:   blog/src/screens/IndexScreen.js

--- blog/src/screens/IndexScreen.js
+++ blog/src/screens/IndexScreen.js
@@ -40,6 +40,16 @@ const IndexScreen = ({ navigation }) => {
   );
 };
 
+IndexScreen.navigationOptions = () => {
+  return {
+    headerRight: () => (
+      <TouchableOpacity onPress={() => navigation.navigate("Create")}>
+        <Feather name="plus" size={30} />
+      </TouchableOpacity>
+    ),
+  };
+}
+
 export default IndexScreen;
 
 const styles = StyleSheet.create({
