0c2a266fce4d6b127f1009fba3b0e64aa9770956
beaucoburn <douglas.coburn@gmail.com>
2024-10-04 17:45:46 -0600

    modified:   blog/src/screens/CreateScreen.js

--- blog/src/screens/CreateScreen.js
+++ blog/src/screens/CreateScreen.js
@@ -25,7 +25,10 @@ const CreateScreen = ({ navigation }) => {
       />
       <Button
         title="Add Blog Post"
-        onPress={() => addBlogPost(title, content)}
+        onPress={() => {
+          addBlogPost(title, content);
+          navigation.navigate("Index");
+        }}
       />
     </View>
   );
