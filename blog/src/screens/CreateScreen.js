89b16c75f340b121fdc4d31b96e184a8b7df89f1
beaucoburn <douglas.coburn@gmail.com>
2024-10-03 18:12:12 -0600

    new file:   blog/src/screens/CreateScreen.js

---
+++ blog/src/screens/CreateScreen.js
@@ -0,0 +1,15 @@
+import React, { useContext } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Context } from "../context/BlogContext";
+
+const ShowScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Create Screen</Text>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({});
+
+export default ShowScreen;
