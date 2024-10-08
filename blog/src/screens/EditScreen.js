3dc1e1c48815abb5d9f07d0b7a3607b673c53a98
beaucoburn <douglas.coburn@gmail.com>
2024-10-08 17:32:33 -0600

    modified:   blog/src/screens/EditScreen.js

--- blog/src/screens/EditScreen.js
+++ blog/src/screens/EditScreen.js
@@ -1,4 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-const  
+const EditScreen = () => {
+  return (
+    <View>
+      <Text>Edit Screen</Text>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({});
+
+export default EditScreen;
