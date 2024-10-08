53ee0da1d8ef33ce33620714ed584a2cf44bb8ea
beaucoburn <douglas.coburn@gmail.com>
2024-10-08 17:52:48 -0600

    modified:   blog/src/screens/EditScreen.js

--- blog/src/screens/EditScreen.js
+++ blog/src/screens/EditScreen.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useContext } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { Context } from '../context/BlogContext';
 
 const EditScreen = ({ navigation }) => {
+  const { state } = useContext(Context);
+
+  const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam('id'));
+
+  const [title, setTitle] = useState(blogPost.title);
+  const [content, setContent] = useState(blogPost.content);
+  
   return (
     <View>
-      <Text>Edit Screen - {navigation.getParam('id')}</Text>
+      <TextInput>Edit Screen - {navigation.getParam('id')}</TextInput>
     </View>
   )
 };
