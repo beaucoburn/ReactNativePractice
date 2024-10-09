d1ee3e2a2306418e7e96bf01b95f6ad095db795d
beaucoburn <douglas.coburn@gmail.com>
2024-10-09 17:21:58 -0600

    new file:   blog/src/components/BlogPostForm.js

---
+++ blog/src/components/BlogPostForm.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const BlogPostForm = () => {
+  
+};
+
+const styles = StyleSheet.create({});
+
+export default BlogPostForm;
