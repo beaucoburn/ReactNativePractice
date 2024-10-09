223ba3c0b2afb1ced433d63ac9a7348ea1991462
beaucoburn <douglas.coburn@gmail.com>
2024-10-09 17:26:02 -0600

    modified:   blog/src/components/BlogPostForm.js 	modified:   blog/src/screens/CreateScreen.js

--- blog/src/components/BlogPostForm.js
+++ blog/src/components/BlogPostForm.js
@@ -2,7 +2,32 @@ import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
 const BlogPostForm = () => {
-  
+  return (
+    <View>
+      <Text style={styles.label}>Enter Title:</Text>
+      <TextInput
+        style={styles.input}
+        value={title}
+        onChangeText={(text) => setTitle(text)}
+        placeholder="Title"
+      />
+      <Text style={styles.label}>Enter Content:</Text>
+      <TextInput
+        style={styles.input}
+        value={content}
+        onChangeText={(text) => setContent(text)}
+        placeholder="Content"
+      />
+      <Button
+        title="Add Blog Post"
+        onPress={() => {
+          addBlogPost(title, content, () => {
+            navigation.navigate("Index");
+          });
+        }}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({});
--- blog/src/screens/CreateScreen.js
+++ blog/src/screens/CreateScreen.js
@@ -6,33 +6,6 @@ const CreateScreen = ({ navigation }) => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const { addBlogPost } = useContext(Context);
-
-  return (
-    <View>
-      <Text style={styles.label}>Enter Title:</Text>
-      <TextInput
-        style={styles.input}
-        value={title}
-        onChangeText={(text) => setTitle(text)}
-        placeholder="Title"
-      />
-      <Text style={styles.label}>Enter Content:</Text>
-      <TextInput
-        style={styles.input}
-        value={content}
-        onChangeText={(text) => setContent(text)}
-        placeholder="Content"
-      />
-      <Button
-        title="Add Blog Post"
-        onPress={() => {
-          addBlogPost(title, content, () => {
-            navigation.navigate("Index");
-          });
-        }}
-      />
-    </View>
-  );
 };
 
 const styles = StyleSheet.create({
