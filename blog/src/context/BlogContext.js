41ec25118562f468624c8b05f30a7de3a9abc8c4
beaucoburn <douglas.coburn@gmail.com>
2024-10-10 17:45:25 -0600

    modified:   blog/src/context/BlogContext.js 	modified:   blog/src/screens/EditScreen.js

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -2,6 +2,10 @@ import createDataContext from "./createDataContext";
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case "edit_blogpost":
+      return state.map((blogPost) => {
+        return blogPost.id === action.payload.id ? action.payload : blogPost;
+      });
     case "delete_blogpost":
       return state.filter((blogPost) => blogPost.id !== action.payload);
     case "add_blogpost":
