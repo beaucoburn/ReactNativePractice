66ee0242f6d41dab1e4fcfb7093df09248b34c4f
beaucoburn <douglas.coburn@gmail.com>
2024-10-10 17:13:26 -0600

    modified:   blog/src/context/BlogContext.js

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -31,8 +31,17 @@ const deleteBlogPost = (dispatch) => {
   };
 };
 
+const editBlogPost = (dispatch) => {
+  return (id, title, content) => {
+    dispatch({
+      type: "edit_blogpost",
+      payload: { id, title, content },
+    });
+  };
+};
+
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost, deleteBlogPost },
+  { addBlogPost, deleteBlogPost, editBlogPost },
   [{ title: "TEST POST", content: "TEST CONTENT", id: 1 }],
 );
