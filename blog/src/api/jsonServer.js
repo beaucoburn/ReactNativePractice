2971b5d98fa74c1fc39fb9595224425e4d6179f3
beaucoburn <douglas.coburn@gmail.com>
2024-10-16 17:51:23 -0600

    modified:   blog/src/api/jsonServer.js

--- blog/src/api/jsonServer.js
+++ blog/src/api/jsonServer.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://b5db-2605-59c8-71f3-6810-00-aa7.ngrok-free.app'
+  baseURL: 'https://b5db-2605-59c8-71f3-6810-00-aa7.ngrok-free.app' // This comes from the ngrok server with npm run tunnel
 });
