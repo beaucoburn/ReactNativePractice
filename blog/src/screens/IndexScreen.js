9927ccdd347d5a49ea0027c7205570c2410725d7
beaucoburn <douglas.coburn@gmail.com>
2024-10-17 17:22:43 -0600

    modified:   blog/src/screens/IndexScreen.js 	modified:   jsonserver/db.json 	modified:   jsonserver/node_modules/ngrok/bin/ngrok

--- blog/src/screens/IndexScreen.js
+++ blog/src/screens/IndexScreen.js
@@ -14,6 +14,10 @@ const IndexScreen = ({ navigation }) => {
 
   useEffect(() => {
     getBlogPosts();
+
+    navigation.addListener('didFocus', () => {
+      getBlogPosts();
+    });
   }, [])
 
   return (
