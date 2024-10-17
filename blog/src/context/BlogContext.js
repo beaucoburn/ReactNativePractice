009fc85e6fcec662a554436a09a9d88be5362a90
beaucoburn <douglas.coburn@gmail.com>
2024-10-17 17:29:52 -0600

    modified:   blog/src/context/BlogContext.js 	modified:   jsonserver/db.json 	modified:   jsonserver/node_modules/ngrok/bin/ngrok

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -11,15 +11,6 @@ const blogReducer = (state, action) => {
       });
     case "delete_blogpost":
       return state.filter((blogPost) => blogPost.id !== action.payload);
-    case "add_blogpost":
-      return [
-        ...state,
-        {
-          id: Math.floor(Math.random() * 99999),
-          title: action.payload.title,
-          content: action.payload.content,
-        },
-      ];
     default:
       return state;
   }
@@ -36,7 +27,6 @@ const getBlogPosts = (dispatch) => {
 const addBlogPost = (dispatch) => {
   return async (title, content, callback) => {
     await jsonServer.post("/blogposts", { title, content });
-    // dispatch({ type: "add_blogpost", payload: { title, content } });
     if (callback) {
       callback();
     }
