ac4660466c4c3bc0cc56267ce15ca7b7a9127b2e
beaucoburn <douglas.coburn@gmail.com>
2024-10-17 17:16:21 -0600

    modified:   blog/src/api/jsonServer.js 	modified:   blog/src/context/BlogContext.js 	modified:   jsonserver/db.json 	modified:   jsonserver/node_modules/ngrok/bin/ngrok

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -34,9 +34,10 @@ const getBlogPosts = (dispatch) => {
 };
 
 const addBlogPost = (dispatch) => {
-  return (title, content, callback) => {
-    dispatch({ type: "add_blogpost", payload: { title, content } });
-    callback();
+  return async (title, content, callback) => {
+    await jsonServer.post("/blogposts", { title, content });
+   // dispatch({ type: "add_blogpost", payload: { title, content } });
+   // callback();
   };
 };
 
