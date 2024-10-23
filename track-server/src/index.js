2d8ef3548d793a44e9f6de38a22b597cefd37e7d
beaucoburn <douglas.coburn@gmail.com>
2024-10-23 17:59:44 -0600

    modified:   track-server/src/index.js

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 const mongoUri = 'mongodb+srv://hello:1D4Ke9ofXippUZUh@cluster0.fp4wh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+mongoose.connect(mongoUri);
+
+mongoose.connection.on('connected', () => {
+  console.log('Connected to mongo instance');
+})
 
 app.get("/", (req, res) => {
   res.send("Hi there!");
