5c4313167e43c592897b18afb08ca3347e93766e
beaucoburn <douglas.coburn@gmail.com>
2024-10-23 17:26:28 -0600

    modified:   track-server/src/index.js

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,7 +1,11 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
-  
+app.get("/", (req, res) => {
+  res.send("Hi there!");
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
 });
