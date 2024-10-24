90300fbaa5eb0d1b33962b6cfb6cda1257c05f31
beaucoburn <douglas.coburn@gmail.com>
2024-10-23 18:01:29 -0600

    modified:   track-server/src/index.js

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -8,7 +8,10 @@ mongoose.connect(mongoUri);
 
 mongoose.connection.on('connected', () => {
   console.log('Connected to mongo instance');
-})
+});
+mongoose.connection.on('error', (err) => {
+  console.error('Error connecting to mongo', err);
+});
 
 app.get("/", (req, res) => {
   res.send("Hi there!");
