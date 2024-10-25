651beb8bd059513b19426b91c11b1ebf5626e19e
beaucoburn <douglas.coburn@gmail.com>
2024-10-25 17:59:18 -0600

    modified:   track-server/src/middlewares/requireAuth.js

--- track-server/src/middlewares/requireAuth.js
+++ track-server/src/middlewares/requireAuth.js
@@ -1,7 +1,11 @@
-const jwt = require('jsonwebtoken');
-const mongoose = require('mongoose');
-const User = mongoose.model('User');
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-}
+
+  if (!authorization) {
+    return res.status(401).send({ error: "You must be logged in." });
+  }
+};
