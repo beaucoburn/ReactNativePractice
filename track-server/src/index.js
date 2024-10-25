9b74286d87a7d6c92adf9d9d2b53f9ab1db0be8f
beaucoburn <douglas.coburn@gmail.com>
2024-10-24 18:31:45 -0600

    modified:   track-server/src/index.js

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
 
 const app = express();
