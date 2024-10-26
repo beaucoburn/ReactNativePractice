3c61812e9e3a10b93cbb9cbc6e4adeb0cdc2d2c5
beaucoburn <douglas.coburn@gmail.com>
2024-10-25 18:08:27 -0600

    modified:   track-server/src/index.js

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,8 +1,9 @@
-require('./models/User');
+require("./models/User");
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
+const requireAuth = require("./middlewares/requireAuth");
 
 const app = express();
 
@@ -20,8 +21,8 @@ mongoose.connection.on("error", (err) => {
   console.error("Error connecting to mongo", err);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hi there!");
+app.get("/", requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
 });
 
 app.listen(3000, () => {
