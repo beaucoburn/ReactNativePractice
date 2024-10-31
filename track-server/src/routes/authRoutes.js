fa9e77f80ba4c0b3663e802c18d5eb3a8369dbb5
beaucoburn <douglas.coburn@gmail.com>
2024-10-31 17:54:20 -0600

    modified:   track-server/src/routes/authRoutes.js

--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -9,25 +9,34 @@ router.post("/signup", async (req, res) => {
   const { email, password } = req.body;
 
   try {
-  const user = new User({ email, password });
-  await user.save();
+    const user = new User({ email, password });
+    await user.save();
 
-  const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
-  res.send({ token });
-  res.send("You made a post request");
-    } catch (err) {
-      return res.status(422).send(err.message);
-    }
-})
+    const token = jwt.sign({ userId: user._id }, "MY_SECRET_KEY");
+    res.send({ token });
+    res.send("You made a post request");
+  } catch (err) {
+    return res.status(422).send(err.message);
+  }
+});
 
 router.post("/signin", async (req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    return res.status(422).send({ error: "Must provide email and password" })
+    return res.status(422).send({ error: "Must provide email and password" });
   }
 
   const user = await User.findOne({ email });
-})
+  if (!user) {
+    return res.status(404).send({ error: "Invalid password or email" });
+  }
+
+  try {
+    await user.comparePassword(password);
+  } catch (err) {
+    return res.status(422).send({ error: "Invalid password or email" });
+  }
+});
 
 module.exports = router;
