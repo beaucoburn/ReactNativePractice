f26aa7b566d16be1673c10fe8d437480791e9299
beaucoburn <douglas.coburn@gmail.com>
2024-10-31 17:50:30 -0600

    modified:   track-server/src/routes/authRoutes.js

--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -20,12 +20,14 @@ router.post("/signup", async (req, res) => {
     }
 })
 
-router.post("/signin", (req, res) => {
+router.post("/signin", async (req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
     return res.status(422).send({ error: "Must provide email and password" })
   }
+
+  const user = await User.findOne({ email });
 })
 
 module.exports = router;
