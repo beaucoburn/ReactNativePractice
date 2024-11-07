15b3e206c22dd880193cb429ae1ec4c71bd2edb7
beaucoburn <douglas.coburn@gmail.com>
2024-11-07 17:33:51 -0600

    modified:   track-server/src/routes/trackRoutes.js

--- track-server/src/routes/trackRoutes.js
+++ track-server/src/routes/trackRoutes.js
@@ -9,5 +9,9 @@ const router = express.Router();
 router.use(requireAuth);
 
 router.get("/tracks", async (req, res) => {
+  const tracks = await Track.find({ userId: req.user._id });
 
+  res.send(tracks);
 })
+
+module.exports = router;
