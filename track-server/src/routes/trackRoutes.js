88520e85eef8f9b941dc83c777cf3dbc85300129
beaucoburn <douglas.coburn@gmail.com>
2024-11-08 18:01:49 -0600

    modified:   track-server/src/routes/trackRoutes.js

--- track-server/src/routes/trackRoutes.js
+++ track-server/src/routes/trackRoutes.js
@@ -21,10 +21,14 @@ router.post("/tracks", async (req, res) => {
     return res.status(422).send({ error: "You must provide a name and locations" });
   }
 
+  try {
   const track = new Track({ name, locations, userId: req.user._id })
   await track.save();
 
   res.send(track);
+  } catch (err) {
+    res.status(422)send({ error: err.message });
+  }
 })
 
 module.exports = router;
