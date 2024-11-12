cfa2b445d1837c440fdf3cd113c8b3a1158e55f3
beaucoburn <douglas.coburn@gmail.com>
2024-11-12 17:24:41 -0600

    modified:   tracks/src/screens/TrackCreateScreen.js 	modified:   tracks/src/screens/TrackDetailScreen.js 	modified:   tracks/src/screens/TrackListScreen.js

--- tracks/src/screens/TrackListScreen.js
+++ tracks/src/screens/TrackListScreen.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const AccountScreen = () => {
+  return (
+    <Text style={{ fontSize: 48 }}>AccountScreen</Text>
+  )
+}
+
+const styles = StyleSheet.create({});
+
+export default AccountScreen;
 
