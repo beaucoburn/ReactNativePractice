191be1c81ea51450de4b30bde1eb2602780723f1
beaucoburn <douglas.coburn@gmail.com>
2024-11-13 17:42:12 -0600

    modified:   tracks/src/screens/SigninScreen.js

--- tracks/src/screens/SigninScreen.js
+++ tracks/src/screens/SigninScreen.js
@@ -1,11 +1,9 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
 
 const SigninScreen = () => {
-  return (
-    <Text style={{ fontSize: 48 }}>SigninScreen</Text>
-  )
-}
+  return <Text style={{ fontSize: 48 }}>SigninScreen</Text>;
+};
 
 const styles = StyleSheet.create({});
 
