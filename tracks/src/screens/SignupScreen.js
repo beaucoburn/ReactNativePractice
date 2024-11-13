ba9d762ee8ea38758043cd64eb4e666eeb00ea90
beaucoburn <douglas.coburn@gmail.com>
2024-11-13 17:31:24 -0600

    modified:   tracks/src/screens/SignupScreen.js

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Button } from 'react-native';
 
-const SignupScreen = () => {
+const SignupScreen = ({ navigation }) => {
   return (
+    <>
     <Text style={{ fontSize: 48 }}>SignupScreen</Text>
+    <Button title="Go to Signin" onPress={() => navigation.navigate('Signin')} />
+    </>
   )
 }
 
