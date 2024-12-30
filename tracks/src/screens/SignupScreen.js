95d1cb2eb43e193c65dddf855ff508c4e5a491e7
beaucoburn <douglas.coburn@gmail.com>
2024-12-30 17:38:20 -0600

    modified:   tracks/src/screens/SignupScreen.js

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -5,7 +5,7 @@ import Spacer from "../components/Spacer";
 
 const SignupScreen = ({ navigation }) => {
   return (
-    <>
+    <View style={styles.container}>
       <Spacer>
         <Text h3>Sign Up for Tracker</Text>
       </Spacer>
@@ -15,7 +15,7 @@ const SignupScreen = ({ navigation }) => {
       <Spacer>
         <Button title="Sign Up" />
       </Spacer>
-    </>
+    </View>
   );
 };
 
@@ -25,6 +25,11 @@ SignupScreen.navigationOptions = () => {
   };
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    borderColor: 'red',
+    borderWidth: 10,
+  }
+});
 
 export default SignupScreen;
