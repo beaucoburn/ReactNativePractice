fd1a64e629290fe65130918e9b6d725e145c52d3
beaucoburn <douglas.coburn@gmail.com>
2024-12-30 17:23:34 -0600

    modified:   tracks/src/components/Spacer.js

--- tracks/src/components/Spacer.js
+++ tracks/src/components/Spacer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 
-const Spacer = () => {
+const Spacer = ({ children }) => {
   return(
     <View style={styles.spacer}>{children}</View>
   )
