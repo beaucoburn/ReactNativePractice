bd97e2af8b1c47fa317e2b37826700423a4d12e6
beaucoburn <douglas.coburn@gmail.com>
2024-12-30 17:44:14 -0600

    modified:   tracks/src/screens/SignupScreen.js

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet } from "react-native";
 import { Text, Input, Button } from "react-native-elements";
 import Spacer from "../components/Spacer";
 
 const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <View style={styles.container}>
       <Spacer>
