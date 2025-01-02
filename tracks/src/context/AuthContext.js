75107e1c030da53ee488c9fb3fbd165b6349b6f4
beaucoburn <douglas.coburn@gmail.com>
2025-01-02 17:31:41 -0600

    modified:   tracks/src/context/AuthContext.js

--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import createDataContext from "~/src/context/DataContext";
+import createDataContext from "./createDataContext";
 
 const authReducer = (state, action) => {
   switch (action.type) {
