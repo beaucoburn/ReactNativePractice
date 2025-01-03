0250f5598471575d7796d60524a48c6a4dcb5a82
beaucoburn <douglas.coburn@gmail.com>
2025-01-03 17:14:51 -0600

    modified:   tracks/src/context/AuthContext.js

--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -3,6 +3,8 @@ import trackerApi from "../api/tracker";
 
 const authReducer = (state, action) => {
   switch (action.type) {
+    case 'add_error':
+      return { ...state, errorMessage: action.payload };
     default:
       return state;
   }
