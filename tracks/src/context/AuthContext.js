84be7fa0c9dc2af0c51d5cf8cf8675b2cc6f3886
beaucoburn <douglas.coburn@gmail.com>
2025-01-08 17:00:55 -0600

    modified:   tracks/src/context/AuthContext.js

--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -6,25 +6,27 @@ const authReducer = (state, action) => {
   switch (action.type) {
     case "add_error":
       return { ...state, errorMessage: action.payload };
-    case 'signup':
+    case "signup":
       return { errorMessage: "", token: action.payload };
     default:
       return state;
   }
 };
 
-const signup = (dispatch) => async ({ email, password }) => {
-    try {
-      const response = await trackerApi.post("/signup", { email, password });
-      await AsyncStorage.setItem('token', response.data.token);
-      dispatch({ type: 'signup', payload: response.data.token });
-    } catch (err) {
-      dispatch({
-        type: "add_error",
-        payload: "Something went wrong with sign up.",
-      });
-    }
-  };
+const signup =
+  (dispatch) =>
+    async ({ email, password }) => {
+      try {
+        const response = await trackerApi.post("/signup", { email, password });
+        await AsyncStorage.setItem("token", response.data.token);
+        dispatch({ type: "signup", payload: response.data.token });
+      } catch (err) {
+        dispatch({
+          type: "add_error",
+          payload: "Something went wrong with sign up.",
+        });
+      }
+    };
 
 const signin = (dispatch) => {
   return ({ email, password }) => {
