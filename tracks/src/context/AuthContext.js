d7d309c574fed1d8d144f6004d0c8eaf4e2099a5
beaucoburn <douglas.coburn@gmail.com>
2025-01-08 17:18:54 -0600

    modified:   tracks/src/context/AuthContext.js

--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -1,6 +1,7 @@
 import createDataContext from "./createDataContext";
 import trackerApi from "../api/tracker";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { navigate } from "../navigationRef";
 
 const authReducer = (state, action) => {
   switch (action.type) {
