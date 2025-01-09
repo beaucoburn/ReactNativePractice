27f23fcd1bd7515332926531f383bb087201e3cb
beaucoburn <douglas.coburn@gmail.com>
2025-01-09 17:19:55 -0600

    new file:   tracks/src/components/AuthForm.js

---
+++ tracks/src/components/AuthForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Text, Button, Input } from 'react-native-elements';
+
+const AuthForm = () => {
+
+};
+
+const styles = StyleSheet.create({});
+
+export default AuthForm;
