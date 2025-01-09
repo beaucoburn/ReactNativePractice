9722d6b26e05fd7584ec790167842756eb6bb0ec
beaucoburn <douglas.coburn@gmail.com>
2025-01-09 17:09:59 -0600

    modified:   tracks/src/screens/SignupScreen.js

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, TouchableOpacity } from "react-native";
 import { Text, Input, Button } from "react-native-elements";
 import Spacer from "../components/Spacer";
 import { Context as AuthContext } from "../context/AuthContext";
