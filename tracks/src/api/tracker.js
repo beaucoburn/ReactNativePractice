f70f6dfd7d347fffb8602513f36debda18bc09cc
beaucoburn <douglas.coburn@gmail.com>
2025-01-10 17:01:05 -0600

    modified:   tracks/src/api/tracker.js

--- tracks/src/api/tracker.js
+++ tracks/src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://ce42-2605-59c8-71f3-6810-00-bc2.ngrok-free.app",
+  baseURL: "https://ce82-2605-59c8-71f3-6810-00-bc2.ngrok-free.app",
 });
