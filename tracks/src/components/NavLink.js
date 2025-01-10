5e0e8f105c82e00d28ac11632aefe1b37bd638c3
beaucoburn <douglas.coburn@gmail.com>
2025-01-10 17:15:32 -0600

    modified:   tracks/src/components/NavLink.js

--- tracks/src/components/NavLink.js
+++ tracks/src/components/NavLink.js
@@ -2,3 +2,11 @@ import React from "react";
 import { Text, TouchableOpacity, StyleSheet } from "react-native";
 import Spacer from "./Spacer";
 import { withNavigation } from "react-navigation";
+
+const NavLink = ({ navigation }) => {
+
+}
+
+const styles = StyleSheet.create({});
+
+export default withNavigation(NavLink);
