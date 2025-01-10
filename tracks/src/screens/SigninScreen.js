98a56ccb52c75429fdf526a68f9074b0c903e2db
beaucoburn <douglas.coburn@gmail.com>
2025-01-10 17:30:30 -0600

    modified:   tracks/src/screens/SigninScreen.js

--- tracks/src/screens/SigninScreen.js
+++ tracks/src/screens/SigninScreen.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { View, StyleSheet, Text } from "react-native";
+import AuthForm from "../components/AuthForm";
+import NavLink from "../components/NavLink";
 
 const SigninScreen = () => {
   return <Text style={{ fontSize: 48 }}>SigninScreen</Text>;
