48b278b02a3af786ec1fa87775bce5c90b57d84e
beaucoburn <douglas.coburn@gmail.com>
2025-01-15 17:43:33 -0600

    modified:   tracks/src/screens/SignupScreen.js

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -10,7 +10,7 @@ const SignupScreen = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <NavigationEvents onWillBlur={clearErrorMessage} />
+      <NavigationEvents onWillFocus={clearErrorMessage} />
       <AuthForm
         headerText="Sign Up for Tracker"
         errorMessage={state.errorMessage}
