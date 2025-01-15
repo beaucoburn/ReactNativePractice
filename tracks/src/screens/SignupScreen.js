4cd16aa1a075492c5caad4dfe72750720aaadd32
beaucoburn <douglas.coburn@gmail.com>
2025-01-15 17:37:19 -0600

    modified:   tracks/src/screens/SignupScreen.js

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { View, StyleSheet } from "react-native";
+import { NavigationEvents } from "react-navigation";
 import { Context as AuthContext } from "../context/AuthContext";
 import AuthForm from "../components/AuthForm";
 import NavLink from "../components/NavLink";
