ee6802cdae8a6e5646c0634fff76cf9b6ddec94f
beaucoburn <douglas.coburn@gmail.com>
2025-01-15 17:39:22 -0600

    modified:   tracks/src/screens/SignupScreen.js

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -6,10 +6,13 @@ import AuthForm from "../components/AuthForm";
 import NavLink from "../components/NavLink";
 
 const SignupScreen = ({ navigation }) => {
-  const { state, signup } = useContext(AuthContext);
+  const { state, signup, clearErrorMessage } = useContext(AuthContext);
 
   return (
     <View style={styles.container}>
+      <NavigationEvents
+        onWillBlur={clearErrorMessage}
+      />
       <AuthForm
         headerText="Sign Up for Tracker"
         errorMessage={state.errorMessage}
