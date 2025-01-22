7ac2f8b9a05dea8aa21548cc8d2104b4522e42c1
beaucoburn <douglas.coburn@gmail.com>
2025-01-21 18:00:17 -0600

    modified:   tracks/App.js

--- tracks/App.js
+++ tracks/App.js
@@ -10,6 +10,7 @@ import TrackDetailScreen from "./src/screens/TrackDetailScreen";
 import TrackListScreen from "./src/screens/TrackListScreen";
 import { Provider as AuthProvider } from "./src/context/AuthContext";
 import { setNavigator } from "./src/navigationRef";
+import ResolveAuthScreen from "./src/screens/ResolveAuthScreen";
 
 const switchNavigator = createSwitchNavigator({
 
